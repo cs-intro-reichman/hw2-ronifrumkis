5c0023c28d85f4bcb46ba270e34aa0215cf91f9f
Roni Frumkis <roni.frumkis@post.runi.ac.il>
2025-11-08 09:40:00 +0200

    done with 3.

--- Cheers.java
+++ Cheers.java
@@ -22,7 +22,7 @@ public class Cheers {
 
             }
 
-            System.out.println("What does  that spell?");
+            System.out.println("What does that spell?");
             for (int i = 0 ; i < N; i++) {
             System.out.println(cheers + "!!!");
             }
